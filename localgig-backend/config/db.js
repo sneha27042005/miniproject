92910ff9c2bf985bc87513a8efa38a5a2df922be
test <test@example.com>
2026-05-27 09:39:33 +0530

    db changes
    LiveReview Pre-Commit Check: ran (iter:1, coverage:0%)

--- localgig-backend/config/db.js
+++ localgig-backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "mysql.railway.internal",
+  host: "zephyr.proxy.rlwy.net",
   user: "root",
   password: "SzKFGNnVnBtAyaDyHpjujDiKPPFgcneE",
   database: "railway",
-  port: 3306
+  port: 19777
 });
 
 db.connect((err) => {
